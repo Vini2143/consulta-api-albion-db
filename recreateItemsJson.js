4cf42fa9a06a381169745fd4b54136d4a845004d
Vinicius <vini2143vinicius@gmail.com>
2023-12-04 19:50:02 -0300

    criado script de retrabalhar a lista de item

--- recreateItemsJson.js
+++ recreateItemsJson.js
@@ -1,7 +1,25 @@
 const fs = require('fs')
 
-let json = JSON.parse(fs.readFileSync('items.json', 'utf-8'))
 
-json.forEach(registro => {
-    pass
-})
+try {
+    let json = JSON.parse(fs.readFileSync('items.json', 'utf-8'))
+
+    let items = []
+
+    json.forEach(registro => {
+
+        let item = {
+        Nome : registro['LocalizedNames']['PT-BR'],
+        Codigo : registro['UniqueName']
+        }
+        
+        items.push(item)
+        
+    })
+
+    let itemsJson = JSON.stringify(items, null, 2);
+
+    fs.writeFileSync('itemsRecriado.json', itemsJson, 'utf8')
+} catch (erro) {
+    console.log(log)
+}
