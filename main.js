9d9331d620f65d6824c7abc1afa80ab41d618b9b
vinicius.prodater <vini2143vinicius@gmail.com>
2023-12-04 09:29:55 -0300

    tratamento dos dados

--- main.js
+++ main.js
@@ -2,10 +2,9 @@ const https = require('https')
 
 let url = 'https://west.albion-online-data.com'
 
-let itemList = 'T4_HIDE_LEVEL3@3'
-let format = 'json'
+let item = 'T4_HIDE_LEVEL3@3'
 
-let parametros  = `/api/v2/stats/Prices/${itemList}.${format}`
+let parametros  = `/api/v2/stats/Prices/${item}.json`
 
 https.get(url + parametros, (resp) => {
   let data = '';
@@ -17,7 +16,12 @@ https.get(url + parametros, (resp) => {
 
   // Toda a resposta foi recebida. Exibir o resultado.
   resp.on('end', () => {
-    console.log(data);
+    dataJson = JSON.parse(data)
+
+    dataJson.forEach((atributo) => {
+        console.log(`${atributo['city']} ${atributo['sell_price_min']} ${atributo['sell_price_max']}`)
+    })
+
   });
 
 }).on("error", (err) => {
